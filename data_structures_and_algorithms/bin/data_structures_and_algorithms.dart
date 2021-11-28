348bf04b44362d23a467db103d5b4f68949a5f83
Emmanuel <adjeimanue@gmail.com>
2021-11-27 22:56:13 -0800

    binary searching algorithm with dart

--- data_structures_and_algorithms/bin/data_structures_and_algorithms.dart
+++ data_structures_and_algorithms/bin/data_structures_and_algorithms.dart
@@ -1,8 +1,8 @@
 import 'package:data_structures_and_algorithms/binary_search.dart';
 
 void main(List<String> arguments) {
-  
+  ///trial array
   var trialArray = [2, 15, 1, 3, 6, 8, 9];
-  
+
   print(Sort().call(trialArray, 4));
 }
